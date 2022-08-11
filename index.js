f9fbe395dc25124fb15c727246750db004b3dffc
agusmar <agusyeah8@gmail.com>
2022-08-11 02:57:11 -0300

    subida

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-let alumnosAprobados = 0;
-let alumnosDesaprobados = 0;
+let alumnosRegistrados = [];
 
 function solicitarAlumno() {
   let nombre = prompt("Ingresar nombre y apellido");
+  let PriCuatri = parseInt (prompt("Ingrese nota del primer cuatrimestre"));
+  let SegCuatri = parseInt (prompt("Ingrese nota del segundo cuatrimestre"));
+  let TerCuatri = parseInt (prompt("Ingrese nota del tercer cuatrimestre"));
 
-  let notaPrimerCuatrimestre = prompt(
-    "Ingrese nota del primer cuatrimestre"
-  );
-  let notaSegundoCuatrimestre = prompt(
-    "Ingrese nota del segundo cuatrimestre"
-  );
-  let notaTercerCuatrimestre = prompt(
-    "Ingrese nota del tercer cuatrimestre"
-  );
+  
 
-  esNotaValida(notaPrimerCuatrimestre, "primer");
-  esNotaValida(notaSegundoCuatrimestre, "segundo");
-  esNotaValida(notaTercerCuatrimestre, "tercero");
 
-  if (
-    notaFinalDelAlumno(
-      notaPrimerCuatrimestre,
-      notaSegundoCuatrimestre,
-      notaTercerCuatrimestre
-    ) > 21
-  ) {
-    alumnosAprobados++;
-    alert("El alumno " + nombre + " se encuentra aprobado");
-  } else {
-    alumnosDesaprobados++;
-    alert(`El alumno ${nombre} se encuentra desaprobado.`);
+
+  let promedio = (esNotaValida(PriCuatri, "primer")+  esNotaValida(SegCuatri, "segundo")+  esNotaValida(TerCuatri, "tercero")) / 3 ;
+
+  alumno = CrearAlumno(nombre, promedio);
+  
+  if ((promedio) > 6) { 
+    alert("El alumno " + nombre + " se encuentra aprobado (" + promedio + ").");
+    alumno.estaAprobado = true;
   }
-}
+  else {
+    alert("El alumno "+ nombre + " se encuentra desaprobado (" + promedio + ").");
+    alumno.estaAprobado = false;
+  }
 
-function notaFinalDelAlumno(primerNota, segundaNota, tercerNota) {
-  let notaFinal = (primerNota + segundaNota + tercerNota) / 3;
-  return notaFinal;
+  alumnosRegistrados.push(alumno);
 }
 
 function esNotaValida(unaNota, cuatrimestre) {
-   
   while ( parseInt(unaNota) < 0 || parseInt(unaNota) > 10 ||  isNaN(parseInt(unaNota)))  {
     unaNota = prompt(
       "ERROR:  Ingrese nota del "+ cuatrimestre + " cuatrimestre, porfavor ingrese numeros del 1 al 10"
     );
   }
+  return parseInt(unaNota);
 }
 
+
+function CrearAlumno (nombreAlumno,promedioAlumno){
+
+  let alumno = {
+    firstName: nombreAlumno,
+    promedioalumno: promedioAlumno,
+    estaAprobado: false,
+  }
+  return alumno;
+};
+
 solicitarAlumno();
+solicitarAlumno();
+
+function obtenerAlumnosAprobados (ListaAlumnos) {
+  const aprobados = ListaAlumnos.filter(item => item.estaAprobado === true );
+  return aprobados;
+}
+function obtenerAlumnosDesaprobados (ListaAlumnos) {
+  const desaprobados = ListaAlumnos.filter(item => item.estaAprobado === false );
+  return desaprobados;
+}
+console.log('Alumnos aprobados: ',obtenerAlumnosAprobados(alumnosRegistrados));
+console.log('Alumnos desaprobados: ',obtenerAlumnosDesaprobados(alumnosRegistrados));
+console.log('Alumnos registrados: ',alumnosRegistrados);
+
+
