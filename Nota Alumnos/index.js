024dffa8150039cd0ab8cd1d54be229618aa5177
agusmar <agusyeah8@gmail.com>
2022-08-26 03:10:35 -0300

    subida

--- Nota Alumnos/index.js
+++ Nota Alumnos/index.js
@@ -1,14 +1,10 @@
-let alumnosRegistrados = [];
+pintarAlumnosStorage();
 
-
-
-function pintarAlumnos(alumnos){
-
+function pintarAlumnosStorage(){
   document.getElementById("table__body").innerHTML = "";
   
-  for (let i = 0; i < alumnos.length; i++) {
-    const alumno = alumnos[i];
-    console.log(alumno);
+  for (let i=1; i <= localStorage.length; i++){
+    const alumno = JSON.parse(localStorage.getItem(i));
 
     const tableRow = document.createElement("tr");
     const rowH = document.createElement("th");
@@ -17,11 +13,11 @@ function pintarAlumnos(alumnos){
     const rowDesaprobado = document.createElement('td');
     const rowPromedio = document.createElement('td');
 
-    rowH.innerHTML =i +  1; 
+    rowH.innerHTML = alumno.id; 
     rowName.innerHTML = alumno.firstName; 
     rowPromedio.innerHTML = alumno.promedioalumno;
     alumno.estaAprobado ? rowAprobado.innerHTML = "✓" : rowDesaprobado.innerHTML = "X"; 
-
+    
     rowH.setAttribute("scope", "row");
     rowH.classList.add('text-center');
     rowName.classList.add('text-center','llamas');
@@ -35,18 +31,16 @@ function pintarAlumnos(alumnos){
     tableRow.appendChild(rowDesaprobado);
     tableRow.appendChild(rowPromedio);
 
-    document.getElementById("table__body").appendChild(tableRow);
+    document.getElementById("table__body").appendChild(tableRow); 
   }
 }
 
 
-
 function solicitarAlumno() {
   let nombre = prompt("Ingresar nombre y apellido");
-  let PriCuatri = parseInt (prompt("Ingrese nota del primer cuatrimestre"));
-  let SegCuatri = parseInt (prompt("Ingrese nota del segundo cuatrimestre"));
-  let TerCuatri = parseInt (prompt("Ingrese nota del tercer cuatrimestre"));
-
+  let PriCuatri = parseInt(prompt("Ingrese nota del primer cuatrimestre"));
+  let SegCuatri = parseInt(prompt("Ingrese nota del segundo cuatrimestre"));
+  let TerCuatri = parseInt(prompt("Ingrese nota del tercer cuatrimestre"));
 
   let promedio = (esNotaValida(PriCuatri, "primer")+  esNotaValida(SegCuatri, "segundo")+  esNotaValida(TerCuatri, "tercero")) / 3 ;
 
@@ -55,17 +49,11 @@ function solicitarAlumno() {
   if ((promedio) >= 7) { 
     alert("El alumno " + nombre + " se encuentra aprobado (" + promedio + ").");
     alumno.estaAprobado = true;
-
-
   }
   else {
     alert("El alumno "+ nombre + " se encuentra desaprobado (" + promedio + ").");
     alumno.estaAprobado = false;
-
   }
-
-  alumnosRegistrados.push(alumno);
-  pintarAlumnos(alumnosRegistrados);
 
 }
 
@@ -76,31 +64,20 @@ function esNotaValida(unaNota, cuatrimestre) {
     );
   }
   return parseInt(unaNota);
-
-
 }
 
 function CrearAlumno (nombreAlumno,promedioAlumno){
-
+  var posicion = localStorage.length;
   let alumno = {
+    id : posicion + 1, 
     firstName: nombreAlumno,
     promedioalumno: promedioAlumno,
     estaAprobado: false,
-  }
+  } 
+  localStorage.setItem(alumno.id,JSON.stringify(alumno));
+  pintarAlumnosStorage();
   return alumno;
 };
-
-function obtenerAlumnosAprobados (ListaAlumnos) {
-  const aprobados = ListaAlumnos.filter(item => item.estaAprobado === true );
-  return aprobados;
-}
-function obtenerAlumnosDesaprobados (ListaAlumnos) {
-  const desaprobados = ListaAlumnos.filter(item => item.estaAprobado === false );
-  return desaprobados;
-}
-console.log('Alumnos aprobados: ',obtenerAlumnosAprobados(alumnosRegistrados));
-console.log('Alumnos desaprobados: ',obtenerAlumnosDesaprobados(alumnosRegistrados));
-console.log('Alumnos registrados: ',alumnosRegistrados);
 
 let boton = document.getElementById("boton");
 boton.addEventListener("click",solicitarAlumno);
